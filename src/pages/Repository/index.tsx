e61508be3a93c45cb1fa7331cb0c29746ecffada
Marcelo Fernandes <marcelo.schreiber@gmail.com>
2020-04-21 18:41:20 -0300

    Style repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -8,10 +12,52 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
   return (
-    <h1>
-      Repository:
-      {params.repository}
-    </h1>
+    <>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/13343185?s=460&u=6f5abe46faf560f37aa41eebb629bf69183e98e3&v=4"
+            alt="lala"
+          />
+          <div>
+            <strong>lalalala</strong>
+            <p>disajdaiojdiao</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>18080</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>18080</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>18080</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="/">
+          <div>
+            <strong>lalala</strong>
+            <p>lelelele</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
